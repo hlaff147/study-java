0b9cc062149fc271cb17b79d4a72783e4571c0ea
Humberto Lima <hlaff@cin.ufpe.br>
2024-02-03 15:38:28 -0300

    Add UserController with the get, post

---
+++ user_mongo/src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.user.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user.model.User;
+import com.example.user.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+}
