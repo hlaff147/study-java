3ba73c664d5a4314b42ab47487760abe361081cd
Humberto Lima <hlaff@cin.ufpe.br>
2024-03-06 16:37:31 -0300

    Add 2011. Final Value of Variable After Performing Operations

--- java-code/Solution.java
+++ java-code/Solution.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
-
 public class Solution {
     public static void main(String[] args) {
     }
-    
+
     // 2469. Convert the Temperature
     public double[] convertTemperature(double celsius) {
         double[] resuArrays = new double[2];
@@ -11,5 +9,18 @@ public class Solution {
         resuArrays[1] = celsius * 1.8 + 32;
         return resuArrays;
     }
-    
+
+    // 2011. Final Value of Variable After Performing Operations
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        for (String operation : operations) {
+            if (operation.equals("--X") || operation.equals("X--")) {
+                --result;
+            } else {
+                ++result;
+            }
+        }
+        return result;
+    }
+
 }
