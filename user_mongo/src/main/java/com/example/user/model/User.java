d08703d7bc4bb19f219b0ff61cc7c8ccfb7ecfc9
Humberto Lima <hlaff@cin.ufpe.br>
2024-11-10 19:53:02 -0300

    Add jakart annotation for validade

--- user_mongo/src/main/java/com/example/user/model/User.java
+++ user_mongo/src/main/java/com/example/user/model/User.java
@@ -1,18 +1,22 @@
 package com.example.user.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import lombok.Data;
 
 @Document
 @Data
 public class User {
     @Id
     private String id;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String email;
+    @NotNull
     private Integer idade;
+    @NotNull
     private Double saldo;
-
 }
